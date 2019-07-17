fdf3a3db7b00c0711196b271e31c09d4f576b2f6
MD. Rakibul Hossain <rakibul.hossain365@gmail.com>
2019-07-18 01:37:37 +0600

    user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from airline.models import Booking
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    fullname = models.CharField(max_length=60)
+    email = models.EmailField(max_length=100, unique=True)
+    age = models.IntegerField(null=True)
+    booked = models.ForeignKey(Booking, on_delete=models.CASCADE)
