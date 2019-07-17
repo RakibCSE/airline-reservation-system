dac21aee6d54ee130639bc8d3bd6254d53e95e50
MD. Rakibul Hossain <rakibul.hossain365@gmail.com>
2019-07-18 01:37:20 +0600

    airline models

--- airline/models.py
+++ airline/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Travelling(models.Model):
+	flight = models.CharField(max_length=60)
+	from_place = models.CharField(max_length=20)
+	to_place = models.CharField(max_length=20)
+	departure_time = models.TimeField()
+	arrival_time = models.TimeField()
+	total_seat = models.IntegerField()
+	available_seat = models.IntegerField()
+	ticket_rate = models.IntegerField()
+	total_collected = models.IntegerField(default=0)
+
+
+class Booking(models.Model):
+	booked = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	no_of_seats = models.IntegerField()
+	Travelling = models.ForeignKey(Travelling, on_delete=models.CASCADE)
+	cost = models.IntegerField(default=0)
