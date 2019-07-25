8672059a59ca09f27d3bd1f3c0bf63ad41d4b49b
RakibCSE <rakibul.hossain365@gmail.com>
2019-07-26 04:49:54 +0600

    model attached

--- airline/admin.py
+++ airline/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 from .models import Travelling, Booking
 
-admin.register(Booking)
-admin.register(Travelling)
+
+admin.site.register(Booking)
+admin.site.register(Travelling)
