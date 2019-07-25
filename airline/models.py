2395331b7391339fc82421d4c48cfae19c149b06
RakibCSE <rakibul.hossain365@gmail.com>
2019-07-26 04:50:17 +0600

    flight image added

--- airline/models.py
+++ airline/models.py
@@ -3,19 +3,23 @@ from django.db import models
 
 
 class Travelling(models.Model):
-	flight = models.CharField(max_length=60)
-	from_place = models.CharField(max_length=20)
-	to_place = models.CharField(max_length=20)
-	departure_time = models.TimeField()
-	arrival_time = models.TimeField()
-	total_seat = models.IntegerField()
-	available_seat = models.IntegerField()
-	ticket_rate = models.IntegerField()
-	total_collected = models.IntegerField(default=0)
+    IMAGE_URL = "https://media.apnarm.net.au/media/images/2018/01/05/" \
+                "imagev172581b44162efca316c368ceb9332a48-de5o2q14aokk572qjp2_ct1880x930.jpg"
+
+    flight = models.CharField(max_length=60)
+    flight_image = models.URLField(max_length=200, default=IMAGE_URL)
+    from_place = models.CharField(max_length=20)
+    to_place = models.CharField(max_length=20)
+    departure_time = models.TimeField()
+    arrival_time = models.TimeField()
+    total_seat = models.IntegerField()
+    available_seat = models.IntegerField()
+    ticket_rate = models.IntegerField()
+    total_collected = models.IntegerField(default=0)
 
 
 class Booking(models.Model):
-	booked = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	no_of_seats = models.IntegerField()
-	Travelling = models.ForeignKey(Travelling, on_delete=models.CASCADE)
-	cost = models.IntegerField(default=0)
+    booked = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    no_of_seats = models.IntegerField()
+    Travelling = models.ForeignKey(Travelling, on_delete=models.CASCADE)
+    cost = models.IntegerField(default=0)
