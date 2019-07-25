2ca4ea88236de128ac410a73f1471d97ff7e0008
RakibCSE <rakibul.hossain365@gmail.com>
2019-07-26 04:51:22 +0600

    booking by model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,4 +10,8 @@ class CustomUser(AbstractUser):
     fullname = models.CharField(max_length=60)
     email = models.EmailField(max_length=100, unique=True)
     age = models.IntegerField(null=True)
+
+
+class BookingBy(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     booked = models.ForeignKey(Booking, on_delete=models.CASCADE)
