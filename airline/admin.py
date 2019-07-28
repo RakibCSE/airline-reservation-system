dc449307b56830235e6d8672cc8a96dda9dba741
RakibCSE <rakibul.hossain365@gmail.com>
2019-07-29 04:30:47 +0600

    travelling admin columns selected

--- airline/admin.py
+++ airline/admin.py
@@ -3,5 +3,24 @@ from django.contrib import admin
 from .models import Travelling, Booking
 
 
-admin.site.register(Booking)
-admin.site.register(Travelling)
+class TravellingAdmin(admin.ModelAdmin):
+    list_display = (
+        "flight",
+        "flight_image",
+        "from_place",
+        "to_place",
+        "departure_time",
+        "arrival_time",
+        "total_seat",
+        "available_seat",
+        "ticket_rate",
+        "total_collected",
+    )
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("booked", "no_of_seats", "Travelling", "cost",)
+
+
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Travelling, TravellingAdmin)
